e13cc6a643a947d164661c5ef3d29695c651fbac
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-09 11:34:39 -0300

    attempting to fix routing

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,14 +75,21 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "endpoint"
+  path_part   = "{proxy+}" #camptura qualquer caminho
 }
 
 # metodo para o endpoint
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
+  http_method   = "ANY" #qualquer metodo
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "root_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id # aponta para a raiz
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -96,13 +103,22 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.lambda_battle_snake_java.invoke_arn
 }
 
+resource "aws_api_gateway_integration" "root_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_method.root_method.resource_id
+  http_method             = aws_api_gateway_method.root_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_battle_snake_java.invoke_arn
+}
+
 # adicionando permiossoes na lambda para o apigateway poder executala
 resource "aws_lambda_permission" "api_gateway_permission" {
   statement_id  = "AllowAPIGatewayToInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_battle_snake_java.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
 
 # nao sei
@@ -128,7 +144,7 @@ resource "aws_api_gateway_stage" "stage" {
 }
 
 # output da url
-output "api_url" {
-  value       = "${aws_api_gateway_stage.stage.invoke_url}/${aws_api_gateway_resource.resource.path_part}"
-  description = "URL para invocar a função Lambda."
+output "api_url_base" {
+  value       = aws_api_gateway_stage.stage.invoke_url
+  description = "URL base da API. Use esta URL no site do Battlesnake."
 }
