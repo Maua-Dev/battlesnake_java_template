fcc9168b16df6a6425260e89ce75d6546ef1286b
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-21 13:42:54 -0300

    fixed dynamodb name, removed created dynamo table

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -48,7 +48,7 @@ resource "aws_s3_bucket_public_access_block" "block_public" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "${var.project_name}-terraform-locks-${var.environment}"
+  name         = replace("${var.project_name}-terraform-locks-${var.environment}", "_", "-")
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
