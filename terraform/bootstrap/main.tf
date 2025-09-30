18398ec120e9a0f4ba9d359bd4b500048a204c1d
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-09-30 10:32:40 -0300

    changing bucket name

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -31,7 +31,7 @@ variable "environment" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "battlesnake-java-template-terraform-state-tfstate"
+  bucket = "battlesnake-java-template-terraform-state-tfstate-new-account"
 
   lifecycle {
     prevent_destroy = true
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_public_access_block" "block_public" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "battlesnake-java-template-terraform-locks-tfstate"
+  name         = "battlesnake-java-template-terraform-locks-tfstate-new-account"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
