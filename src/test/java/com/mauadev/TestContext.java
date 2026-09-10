f2740d5abafb5dd16e274b7012385d050ab143c3
Leonardo Moreno <leonardoa.moreno07@gmail.com>
2026-09-10 19:37:46 -0300

    fix: correct Java template tests and logic

--- src/test/java/com/mauadev/TestContext.java
+++ src/test/java/com/mauadev/TestContext.java
@@ -6,17 +6,69 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 public class TestContext implements Context {
-    @Override public String getAwsRequestId() { return "test-request-id"; }
-    @Override public String getLogGroupName() { return "/aws/lambda/test"; }
-    @Override public String getLogStreamName() { return "test-stream"; }
-    @Override public String getFunctionName() { return "test-function"; }
-    @Override public String getFunctionVersion() { return "1"; }
-    @Override public String getInvokedFunctionArn() { return "arn:test"; }
-    @Override public CognitoIdentity getIdentity() { return null; }
-    @Override public ClientContext getClientContext() { return null; }
-    @Override public int getRemainingTimeInMillis() { return 5000; }
-    @Override public int getMemoryLimitInMB() { return 512; }
-    @Override public LambdaLogger getLogger() {
-        return message -> System.out.println("[LAMBDA] " + message);
+
+    @Override
+    public String getAwsRequestId() {
+        return "test-request-id";
+    }
+
+    @Override
+    public String getLogGroupName() {
+        return "test-log-group";
+    }
+
+    @Override
+    public String getLogStreamName() {
+        return "test-log-stream";
+    }
+
+    @Override
+    public String getFunctionName() {
+        return "battlesnake-test";
+    }
+
+    @Override
+    public String getFunctionVersion() {
+        return "1";
+    }
+
+    @Override
+    public String getInvokedFunctionArn() {
+        return "arn:aws:lambda:test";
+    }
+
+    @Override
+    public CognitoIdentity getIdentity() {
+        return null;
+    }
+
+    @Override
+    public ClientContext getClientContext() {
+        return null;
+    }
+
+    @Override
+    public int getRemainingTimeInMillis() {
+        return 30000;
+    }
+
+    @Override
+    public int getMemoryLimitInMB() {
+        return 512;
+    }
+
+    @Override
+    public LambdaLogger getLogger() {
+        return new LambdaLogger() {
+            @Override
+            public void log(String message) {
+                System.out.print(message);
+            }
+
+            @Override
+            public void log(byte[] message) {
+                System.out.print(new String(message));
+            }
+        };
     }
 }
